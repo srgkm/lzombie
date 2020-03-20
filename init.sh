94e2b1e40c3ba8571792ca13412faab286bd53fc
Sergey Kim <srgkm@yandex-team.ru>
2020-03-20 11:30:41 +0300

    init

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo -n true
+
+apt-get update
+apt-get install -y openssh-server cups supervisor git vim curl
+
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python3.6 python3.6-venv
+
+cd || exit
+
+git clone https://github.com/srgkm/lzombie.git
+
+cd lzombie || exit
+
+python3.6 -m venv .venv
+
+.venv/bin/pip install -U pip
+.venv/bin/pip install -r requirements.txt
+
+cd || exit
+chown -R "$(id -u)":"$(id -g)" lzombie
