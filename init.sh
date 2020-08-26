c6c39428e047d5ea779f7e91ac8ea46868dc67c0
Sergei Kim <srg.kim@gmail.com>
2020-08-26 13:19:41 +0300

    Update init.sh
    для убунты 18

--- init.sh
+++ init.sh
@@ -7,19 +7,19 @@ sudo -n true
 apt-get update
 apt-get install -y openssh-server cups supervisor git vim curl
 
-add-apt-repository -y ppa:deadsnakes/ppa
+# add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
-apt-get install -y python3.6 python3.6-venv
+apt-get install -y python3 python3-pip python3-venv
 
 cd /home/zombielavka
 
-rm /home/zombielavka/lzombie || true
+rm -rf /home/zombielavka/lzombie || true
 
 git clone https://github.com/srgkm/lzombie.git
 
 cd lzombie
 
-python3.6 -m venv .venv
+python3 -m venv .venv
 
 .venv/bin/pip install -U pip
 .venv/bin/pip install -r requirements.txt
